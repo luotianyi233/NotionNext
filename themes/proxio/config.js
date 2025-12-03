4a0e96366353e2ba9a7e01157da1ec97426cd023
Ricky Wang <91827325+luotianyi233@users.noreply.github.com>
2025-12-03 05:26:57 -0500

    Change blog count display from 8 to 4

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -24,7 +24,7 @@ const CONFIG = {
   // 文章区块
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
   PROXIO_BLOG_TITLE: 'Featured Projects',
-  PROXIO_BLOG_COUNT: 8, // 首页博文区块展示前4篇文章
+  PROXIO_BLOG_COUNT: 4, // 首页博文区块展示前4篇文章
   PROXIO_BLOG_TEXT_1: 'Explore my latest selected works',
 
   // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
